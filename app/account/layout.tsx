f8459c1b3e4cc2f6b3efba64942e278ecf8c79f5
Mirwais <mirwais.sanaie@gmail.com>
2025-07-31 13:31:47 +0430

    ...

---
+++ app/account/layout.tsx
@@ -0,0 +1,12 @@
+import SideNavigation from "../_components/SideNavigation";
+
+function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="grid grid-cols-[16rem_1fr] gap-12 h-full">
+      <SideNavigation />
+      <div className="py-1">{children}</div>
+    </div>
+  );
+}
+
+export default layout;
