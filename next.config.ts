905cacaf5b7c99f9e4c2311873fca8aea38a58be
Mirwais <mirwais.sanaie@gmail.com>
2025-08-03 12:52:43 +0430

    ...

--- next.config.ts
+++ next.config.ts
@@ -11,14 +11,6 @@ const nextConfig = {
       },
     ],
   },
-
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
